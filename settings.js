ef27175e03666c741fc0f2b139a562347ff8770d
Munawar1366 <160379958+Munawar1366@users.noreply.github.com>
2024-02-18 23:54:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,10 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6966664324:AAFdOLyli1C9L6b8vgTekH3INos9f-NE4is" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "BotGabutDoang" //your bot name
+global.OWNER_NAME = "𝐆𝐄𝐑𝐀𝐍𝐃𝐎𝐍𝐆😈" //your name
+global.OWNER = ["https://t.me/@JUNGLER01", "https://t.me/@JUNGLER01"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
